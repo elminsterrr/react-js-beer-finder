fd39b41d5322ec777fdf1bc6c754f1786aeb0231
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-13 20:34:52 +0100

    Add 'h1' Beer Finder

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div />
+      <div>
+        <h1>Beer Finder</h1>
+      </div>
     )
   }
 }
