ad91a68e78294969e9059e55b668db02d1a17d5d
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-14 09:47:13 +0100

    Add 'redux' to the project
    npm i -S redux
    npm i -S react-redux
    npm i -S redux-thunk
    npm i redux-devtools-extension --save-dev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-render(<App />, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
