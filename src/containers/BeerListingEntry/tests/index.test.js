f136a4b3d3a88852096d5d7e40d7f92a93438115
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-15 01:54:36 +0100

    Fix Jest error regarding store

--- src/containers/BeerListingEntry/tests/index.test.js
+++ src/containers/BeerListingEntry/tests/index.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { shallow } from 'enzyme';
+import reducers from '../../../reducers';
+
 import BeerListingEntry from '../';
 
+const store = createStore(reducers);
+
 describe('BeerListingEntry', () => {
-  const beerListingEntry = shallow(<BeerListingEntry />);
+  const beerListingEntry = shallow(
+    <Provider store={store}>
+      <BeerListingEntry />
+    </Provider>
+  );
 
   it('renders correctly', () => {
     expect(beerListingEntry).toMatchSnapshot();
