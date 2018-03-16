6d6fce0bf15c6c9bc68a6a1733711ce1a59417e0
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-16 20:47:57 +0100

    Change 'BeerListItem'

--- src/components/BeersListItem/index.js
+++ src/components/BeersListItem/index.js
@@ -3,24 +3,22 @@ import Card, { CardContent } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import './style.css';
 
-function BeerListItem(props) {
-  return (
-    <div>
-      <Card raised className="BeerListItem-main-card">
-        <CardContent>
-          <img
-            src={props.beer.image_url}
-            alt="beer"
-            className="BeerListItem-img"
-          />
-          <Typography variant="headline" component="h2">
-            {props.beer.name}
-          </Typography>
-          <Typography component="p">{props.beer.tagline}</Typography>
-        </CardContent>
-      </Card>
-    </div>
-  );
-}
+const BeerListItem = props => (
+  <div>
+    <Card raised className="BeerListItem-main-card">
+      <CardContent>
+        <img
+          src={props.beer.image_url}
+          alt="beer"
+          className="BeerListItem-img"
+        />
+        <Typography variant="headline" component="h2">
+          {props.beer.name}
+        </Typography>
+        <Typography component="p">{props.beer.tagline}</Typography>
+      </CardContent>
+    </Card>
+  </div>
+);
 
 export default BeerListItem;
