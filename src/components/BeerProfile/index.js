290e74db2da7c884d0d3c05ddf195e0fecd0ea96
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-19 00:54:42 +0100

    BeerProfile styling

--- src/components/BeerProfile/index.js
+++ src/components/BeerProfile/index.js
@@ -21,10 +21,8 @@ class BeerProfile extends Component {
 
   render() {
     const { beer } = this.props;
-    console.log(beer.food_pairing);
     return (
       <div className="BeerProfile-main-container">
-
         <div className="BeerProfile-img-container">
           <img src={beer.image_url} className="BeerProfile-img" alt="beer" />
         </div>
@@ -33,10 +31,20 @@ class BeerProfile extends Component {
           <h2 className="BeerProfile-h2-name">{beer.name}</h2>
           <p className="BeerProfile-tagline">{beer.tagline}</p>
           <hr className="BeerProfile-hr" />
-          <p className="BeerProfile-scales">IBU: {beer.ibu}, ABV: {beer.abv}%, EBC: {beer.ebc}</p>
+          <p className="BeerProfile-scales">
+            <span className="BeerProfile-scales-span">IBU:</span>{' '}
+            <span className="BeerProfile-scales-data">{beer.ibu} </span>
+            <span className="BeerProfile-scales-span">ABV:</span>{' '}
+            <span className="BeerProfile-scales-data">{beer.abv}% </span>
+            <span className="BeerProfile-scales-span">EBC:</span>{' '}
+            <span className="BeerProfile-scales-data">{beer.ebc}</span>
+          </p>
           <p className="BeerProfile-description">{beer.description}</p>
+          <p className="BeerProfile-description">
+            <span className="BeerProfile-brewers-tips">Brewers tips:</span>{' '}
+            {beer.brewers_tips}
+          </p>
         </div>
-
       </div>
     );
   }
