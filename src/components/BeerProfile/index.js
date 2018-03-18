0e11a5adbb1d69a3acb98f72acd83c83a1d6fbd8
elminsterrr <przemyslaw.glab@gmail.com>
2018-03-18 14:21:03 +0100

    Add more details to BeerProfile

--- src/components/BeerProfile/index.js
+++ src/components/BeerProfile/index.js
@@ -21,17 +21,19 @@ class BeerProfile extends Component {
 
   render() {
     const { beer } = this.props;
-    // console.log(beer.image_url); <p>{beer.id}</p>
+    console.log(beer.food_pairing);
     return (
       <div className="BeerProfile-main-container">
 
         <div className="BeerProfile-img-container">
           <img src={beer.image_url} className="BeerProfile-img" alt="beer" />
         </div>
+
         <div className="BeerProfile-text-container">
           <h2 className="BeerProfile-h2-name">{beer.name}</h2>
           <p className="BeerProfile-tagline">{beer.tagline}</p>
           <hr className="BeerProfile-hr" />
+          <p className="BeerProfile-scales">IBU: {beer.ibu}, ABV: {beer.abv}%, EBC: {beer.ebc}</p>
           <p className="BeerProfile-description">{beer.description}</p>
         </div>
 
